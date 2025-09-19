cf0d8ddf74fed924a268fde228eabf0a69b64e0c
Shreyash <cyberninjax45@gmail.com>
2025-09-19 22:47:10 +0530

    Day-40

--- Day-40/Q143_Reorder_List.cpp
+++ Day-40/Q143_Reorder_List.cpp
@@ -11,7 +11,7 @@ L0 → L1 → … → Ln-1 → Ln
 
 Reorder the list into the form:
 L0 → Ln → L1 → Ln-1 → L2 → Ln-2 → …
-
+ 
 Constraints:
 - You cannot change node values, only rearrange nodes.
 - Example 1:
@@ -141,6 +141,7 @@ int main() {
 
 // Input: [1,2,3,4,5]
 // Output: [1,5,2,4,3]
+
 
 
 
